416ad6941e09be10a287f37e4489f65be067de27
fateme roozyani <fzzhroozyani@gmail.com>
2025-10-18 00:22:37 +0330

    The first phase

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+def recv(conn):
+    while True:
+        try:
+            data =conn.recv(1024)
+            if not data:
+                print("disconection for server..")
+                break
+            print(data.decode())
+        except:
+            print("Error")
+            break
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(("127.0.0.1", 60000))
+        username = input("Please enter username: ")
+        s.sendall(username.encode())
+        t = threading.Thread(target=recv, args=(s,))
+        t.start()
+        while True:
+            try:
+                message= input()
+                s.sendall(message.encode())
+            except Exception as e:
+                print(f"Error : {e}")
+                break
+main()
---
+++ server.py
@@ -0,0 +1,48 @@
+import socket 
+import threading
+import time
+members=[]
+user_names=[]
+def connection(conn):
+    global members
+    with conn:
+        user_name= conn.recv(1024).decode('utf-8')
+
+        members.append(conn)
+        user_names.append(user_name)
+
+        message_join=" joined the chat room"
+        send(user_name,message_join,conn)
+        while True:
+            try:
+                message = conn.recv(1024).decode('utf-8')
+                if not message:
+                    break
+                if message =="list":
+                    message=', '.join(user_names) 
+                send(user_name,message, conn)
+            except:
+                break
+            
+def send (user_name,message,conn):
+    global members
+    for client in members:
+              
+            time_now=str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+            messages=user_name+":"+message+"\n"+time_now
+
+            client.sendall(messages.encode('utf-8'))
+
+    print(messages)            
+def many():
+       
+    HOST= '127.0.0.1'
+    PORT=60000
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
+        s.listen()
+        while True:
+            conn,addr= s.accept()
+            t=threading.Thread(target=connection,args=(conn,))
+            t.start()
+many()
